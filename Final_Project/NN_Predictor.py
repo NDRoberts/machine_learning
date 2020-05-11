e55c9507c9a4d6362ac3429e7c322f81e87095ac
Nate Roberts <robernat@msudenver.edu>
2020-05-10 22:56:49 -0600

    Minor cleanup

--- Final_Project/NN_Predictor.py
+++ Final_Project/NN_Predictor.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.metrics import mean_squared_error, r2_score, make_scorer
-from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.metrics import mean_squared_error  # , make_scorer
+from sklearn.model_selection import KFold       # , GridSearchCV
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
 
@@ -58,29 +58,33 @@ class NN_Predictor:
         self.data.drop(columns=drop_cols, inplace=True)
         self.data = self.data.applymap(lambda x: np.NaN if x == -9 else x)
         self.data = self.data.loc[:, ['NO3', 'SO4', 'Cl', 'NH4']]
-        self.data = self.data.query('NO3 != "NaN" & SO4 != "NaN" & Cl != "NaN" & NH4 != "NaN"')
+        self.data = self.data.query('NO3 != "NaN" & SO4 != "NaN"'
+                                    + '& Cl != "NaN" & NH4 != "NaN"')
         scaler = StandardScaler()
         scaler.fit(np.array(self.data))
         self.std_data = pd.DataFrame(scaler.transform(self.data))
 
     def build(self):
-        mlpParams = {
-            "hidden_layer_sizes": vec_combine(
-                [n for n in range(10, 101) if n % 10 == 0],
-                [n for n in range(10, 101) if n % 10 == 0]
-            ),
-            "activation": ["logistic", "tanh", "relu"],
-            "solver": ["sgd", "adam"],
-            "alpha": [0.005, 0.001, 0.0001]
-        }
+        # mlpParams = {
+        #     "hidden_layer_sizes": vec_combine(
+        #         [n for n in range(10, 101) if n % 10 == 0],
+        #         [n for n in range(10, 101) if n % 10 == 0]
+        #     ),
+        #     "activation": ["logistic", "tanh", "relu"],
+        #     "solver": ["sgd", "adam"],
+        #     "alpha": [0.005, 0.001, 0.0001]
+        # }
         self.model = MLPRegressor(hidden_layer_sizes=(15, 12, 9, 6, 3),
-                           activation="relu",
-                           solver='adam',
-                           alpha=0.001,
-                           learning_rate='adaptive',
-                           max_iter=10000,
-                           shuffle=True, )
-        # reg = GridSearchCV(estimator=mlp, param_grid=mlpParams, n_jobs=-1, scoring=make_scorer(mean_squared_error))
+                                  activation="relu",
+                                  solver='adam',
+                                  alpha=0.001,
+                                  learning_rate='adaptive',
+                                  max_iter=10000,
+                                  shuffle=True)
+        # reg = GridSearchCV(estimator=mlp,
+        #                    param_grid=mlpParams,
+        #                    n_jobs=-1,
+        #                    scoring=make_scorer(mean_squared_error))
         # reg.fit(X=self.X, y=np.ravel(self.y))
         # print(reg.best_params_)
         # self.model.fit(self.X, np.ravel(self.y))
@@ -89,14 +93,14 @@ class NN_Predictor:
         # self.predictions = self.model.predict(self.X)
         # print("MSE =", mean_squared_error(self.y, self.predictions))
         # print("R^2 =", r2_score(self.y, self.predictions))
+
+    def plotz(self):
         total_error = 0
         rem_plts = [231, 232, 233, 234, 235, 236]
         # fig, axs = plt.subplots(2, 3, sharex=True, sharey=True)
         for fold in self.folded_data:
-            print(fold["test_y"].shape)
             self.model.fit(fold["train_X"], np.ravel(fold["train_y"]))
             pred = self.model.predict(fold["test_X"])
-            print(pred.shape)
             plt.subplot(rem_plts[0])
             plt.plot(fold["test_X"], fold["test_y"], 'bs', alpha=0.5)
             plt.plot(fold["test_X"], pred, 'go', alpha=0.5)
@@ -110,12 +114,10 @@ class NN_Predictor:
         # plt.plot(self.X, avg_preds, 'g^', alpha = 0.5)
         plt.show()
         print("Average MSE:", total_error / 5)
-            
 
 
 if __name__ == "__main__":
     print("Duh, I'm a brain machine.")
-    bungus = NN_Predictor("NTN-NE15-w.csv")
-    # print(bungus.folded_data[0]["train_X"])
-    # print(bungus.folded_data[0]["train_y"])
-    bungus.build()
+    unit = NN_Predictor("NTN-NE15-w.csv")
+    unit.build()
+    unit.plotz()
