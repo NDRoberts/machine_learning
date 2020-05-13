c0b45e520a14de8d7845d5337f0e58a67c1f0c96
Nathaniel Roberts <robernat@msudenver.edu>
2020-05-12 23:36:51 -0600

    Merging in Ryan's changes; also added results plot

--- Final_Project/FinalProject_RyanMcCullough_NathanielRoberts.py
+++ Final_Project/FinalProject_RyanMcCullough_NathanielRoberts.py
@@ -9,48 +9,44 @@ import os
 import numpy as np
 import pandas as pd
 import keras
-import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.model_selection import KFold, train_test_split
 from matplotlib import pyplot as plt
-from sklearn.preprocessing import StandardScaler
 
 
 def loadData():
-    ''' Read a .CSV file into memory and return it as a NumPy array '''
+    ''' Read a .CSV file into memory and return it as a Pandas dataframe '''
     cwpath = "/ne15.csv"
     dpath = os.getcwd() + cwpath
-
     print("Loading Data ...")
     data = pd.read_csv(dpath)
-
     return data
 
 
 def cropData(data):
-    ''' Extract only relevant columns, and only rows for which relevant
-        data is complete '''
-    drop_cols = ["flagCa", "flagMg", "flagK", "flagNa", "flagNH4",
+        ''' Exclude data irrelevant to this operation, as well as entries
+            for which relevant data is incomplete '''
+        drop_cols = ["flagCa", "flagMg", "flagK", "flagNa", "flagNH4",
                      "flagNO3", "flagCl", "flagSO4", "flagBr", "valcode",
                      "invalcode"]
-    data.drop(columns=drop_cols, inplace=True)
-    data = data.applymap(lambda x: np.NaN if x == -9 else x)
-    data = data.loc[:, ['NO3', 'SO4', 'Cl', 'NH4']]
-    data = data.query('NO3 != "NaN" & SO4 != "NaN"'
-                        + '& Cl != "NaN" & NH4 != "NaN"')
-    return data
+        data.drop(columns=drop_cols, inplace=True)
+        data = data.applymap(lambda x: np.NaN if x == -9 else x)
+        data = data.loc[:, ['NO3', 'SO4', 'Cl', 'NH4']]
+        data = data.query('NO3 != "NaN" & SO4 != "NaN" &'
+                          + 'Cl != "NaN" & NH4 != "NaN"')
+        return data
 
 
 def splitData(data, fold=0):
     ''' Divide data into Testing, Validation, and Training segments or,
         if a fold value is provided, into k cross-validation folds '''
-    data = crop_and_regularize(data)
-    print("Here")
-    print(data)
+    data = cropData(data)
     data = np.array(data)
     X = data[:, :-1]
     y = data[:, -1:]
+    y = np.ravel(y)
+    # y = np.reshape(y, (1447,1))
     if fold > 0:
         kfolds = KFold(n_splits=fold, shuffle=True)
         folded_data = []
@@ -70,58 +66,56 @@ def splitData(data, fold=0):
 
 def trainModel(data):
     ''' Construct and train the artificial neural network model '''
+    (xTest, xTrain, xVal, yTrain, yTest, yVal) = splitData(data)
+
     model = Sequential()
-    model.add(Dense(15, activation='relu', input_shape=(3,)))
-    model.add(Dense(12, activation='relu'))
-    model.add(Dense(9, activation='relu'))
-    model.add(Dense(6, activation='relu'))
-    model.add(Dense(3, activation='relu'))
-    model.add(Dense(1, activation='softmax'))
-    model.compile(optimizer='adagrad', loss='categorical_crossentropy')
+    model.add(Dense(15, activation='linear', input_shape=(3,)))
+    model.add(Dense(12, activation='linear'))
+    model.add(Dense(9, activation='linear'))
+    model.add(Dense(6, activation='linear'))
+    model.add(Dense(3, activation='linear'))
+    model.add(Dense(1, activation='linear'))
+    model.compile(optimizer='adagrad', loss='mean_squared_logarithmic_error')
     
+    keras.utils.plot_model(model, to_file="./ModelStruct.png", show_shapes=True)
 
-    (xTest, xTrain, xVal, yTest, yTrain, yVal) = splitData(data)
-    print(xTrain.shape)
+    history = model.fit(xTrain, yTrain, epochs=10, validation_data=(xVal, yVal))
     
+    yPred = np.array(model.predict(xTest))
+
     print(model.summary())
-
-    trained = model.fit(xTrain, yTrain)
-
-    return trained, xTest, yTest
-
-def crop_and_regularize(data):
-        ''' Exclude data irrelevant to this operation; also create
-            standardized versions of the data I guess?  Not using them yet. '''
-        drop_cols = ["flagCa", "flagMg", "flagK", "flagNa", "flagNH4",
-                     "flagNO3", "flagCl", "flagSO4", "flagBr", "valcode",
-                     "invalcode"]
-        data.drop(columns=drop_cols, inplace=True)
-        data = data.applymap(lambda x: np.NaN if x == -9 else x)
-        data = data.loc[:, ['NO3', 'SO4', 'Cl', 'NH4']]
-        data = data.query('NO3 != "NaN" & SO4 != "NaN"'
-                                    + '& Cl != "NaN" & NH4 != "NaN"')
-        scaler = StandardScaler()
-        scaler.fit(np.array(data))
-        std_data = pd.DataFrame(scaler.transform(data))
-        return std_data
-
-
-def predictions(tModel, xTest, yTest):
-    ''' Make and print predictions using the trained model and test data '''
-    yPred = np.array(tModel.predict(xTest))
+    
+    '''
     for i in range(len(yPred)):
-        print("Actual value: " + yTest[i] + "\n")
-        print("Predicted value: " + yPred[i] + "\n")
+        print("Actual value: ")
+        print(yTest[i][0])
+        print("\n")
+        print("Predicted value: ") 
+        print(yPred[i])
+        print("\n")'''
 
-def plotit(xdata, *ydata):
-    ''' Shorthand method to plot multiple data sets with shared X values
-        (e.g. true y vs predicted y) '''
-    for bit in ydata:
-        plt.plot(xdata, bit, alpha=0.3)
+    return (history, xTest, yTest, yPred)
+
+
+def fullPlot(X_data, y_true, y_pred):
+    base = ['rh', 'gh', 'bh']
+    elem = ['NO3', 'SO4', 'Cl']
+    plt.figure(figsize=(16, 9))
+    plt.suptitle("NH4 Concentrations: Observations v. Predictions")
+    for n in range(3):
+        ploc = 130 + (n + 1)
+        plt.subplot(ploc)
+        plt.xlabel(elem[n])
+        plt.plot(X_data[:, n], y_true, base[n], alpha=0.3,
+                 label=f"NH4 relative to {elem[n]} (observed)")
+        plt.plot(X_data[:, n], y_pred, 'xk', alpha=0.3,
+                 label=f"NH4 relative to {elem[n]} (predicted)")
+        plt.legend()
+    plt.savefig("./results.png", format='png')
     plt.show()
 
 
 if __name__ == '__main__':
-    data = splitData(cropData(loadData()))
-    (trainedModel, xTest, yTest) = trainModel(data)
-    predictions(trainedModel, xTest, yTest)
+    data = loadData()
+    (trainedModel, xTest, yTest, yPred) = trainModel(data)
+    fullPlot(xTest, yTest, yPred)
